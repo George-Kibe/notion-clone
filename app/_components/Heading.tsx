1655c287035985d01f6b6eb4ff17d6ba3ecae5f5
George-Kibe <georgekibewambui@gmail.com>
2023-10-18 19:35:51 +0300

    footer

--- app/_components/Heading.tsx
+++ app/_components/Heading.tsx
@@ -1,12 +1,28 @@
 "use client"
 
+import { Button } from '@/components/ui/button'
+import { ArrowRight } from 'lucide-react'
+import Link from 'next/link'
 import React from 'react'
 
 const Heading = () => {
   return (
     <div className='max-w-3xl space-y-4'>
-      <h1>Your Ideas, Documents & Plans. Unified. Welcome</h1>
-      <span className="underline">RealNotion</span>
+      <h1 className='text-2xl sm:text-3xl md:text-4xl font-bold'>
+        Your Ideas, Documents & Plans. Unified. Welcome to 
+        <span className="underline ml-2">RealNotion</span>
+      </h1>
+      <h3 className="text-base sm:text-xl md:text-2xl font-medium">
+        Jotion is the connected workspace where <br />
+        better, faster work happens.
+      </h3>
+      <Button asChild>
+          <Link href="/documents">
+            Enter Jotion
+            <ArrowRight className="h-4 w-4 ml-2" />
+          </Link>
+        </Button>
+      
     </div>
   )
 }
